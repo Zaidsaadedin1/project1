599169e8c450e5392cbc7bae84fb306115d54cf1
zaid <zaidsaadedin11@gmail.com>
2023-05-23 06:50:30 +0300

    master reset the main repo

--- MasterProject/src/Main.java
+++ MasterProject/src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-
-        System.out.println(" ziad added this line ");
-        System.out.println("monty");
-
+        
     }
 }
